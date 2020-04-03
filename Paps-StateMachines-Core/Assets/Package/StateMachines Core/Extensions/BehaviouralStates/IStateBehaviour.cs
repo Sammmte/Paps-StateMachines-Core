d70737a10a1526da95c659c6bb8ef42e7fc3e2ad
Santiago Pérez <santiagoperez.-@hotmail.com>
2020-04-03 07:48:02 -0300

    added previous plain state machine domain

---
+++ Paps-StateMachines-Core/Assets/Package/StateMachines Core/Extensions/BehaviouralStates/IStateBehaviour.cs
@@ -0,0 +1,9 @@
+﻿namespace Paps.StateMachines.Extensions.BehaviouralStates
+{
+    public interface IStateBehaviour
+    {
+        void OnEnter();
+        void OnUpdate();
+        void OnExit();
+    }
+}
